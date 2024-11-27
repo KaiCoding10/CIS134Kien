1104daf8efcb1fabf24d1db9f3b50e62c1a4f7c8
KaiCoding10 <nguyennhatkien247@gmail.com>
2024-11-26 22:22:00 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Your Business</title>
+    <link rel="stylesheet" href="CSS/sharedStyle.css"
+</head>
+<body>
+<header>
+    <img src="Media/istockphoto-1053291038-612x612.jpg" alt="Our Business" />
+    <h1>Welcome to ProDev Business</h1>
+    <p>Your Partner in Growth</p>
+</header>
+
+<nav>
+    <a href="index.php">Home</a>
+    <a href="about.php">About Us</a>
+    <a href="services.php">Services</a>
+    <a href="contact.php">Contact</a>
+</nav>
+
+<div class="container">
+    <h2>"We are excited to join you on your journey"</h2>
+    <p>
+        At ProDev, we are dedicated to providing the best solutions for your needs.
+        With years of experience in web development, we offer services such as responsive web design, e-commerce platforms, custom web applications, content management systems, and website optimization. We stay ahead of industry trends, leveraging the latest technologies to ensure your online presence is fast, secure, and optimized for search engines.
+    </p>
+    <h2>Join us to grow your business and achieve your dreams.</h2>
+<!-- using php echo to output text in HTML page-->
+    <?php echo "Our team of professionals is here to help you every step of the way. Whether you need a new responsive and eye-catching website or bring your current website to the next level we got you covered, ProDev has the expertise you need."?>
+
+</div>
+
+<footer>
+<!--    strtoupper() php built-in function convert string to UPPERCASE-->
+    <?php echo strtoupper('&copy; 2024 ProDev LLC. All Rights Reserved.'); ?>
+</footer>
+</body>
+</html>
+
